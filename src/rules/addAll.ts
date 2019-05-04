8ef7d54a3b451884bc20e9dd0bf926329278006c
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-05-04 17:16:21 +0100

    Add comment on addAll rule

--- src/rules/addAll.ts
+++ src/rules/addAll.ts
@@ -2,6 +2,11 @@ const initialAcc = {
   amount: 0
 };
 
+/*
+ * addAll sums all amounts in the basket. Might be the
+ * minimum rule to implement for a cart :)
+ */
+
 const addAll = (): Basket.Rule => (acc, item): Basket.RuleResult => {
   const { code } = item;
 
