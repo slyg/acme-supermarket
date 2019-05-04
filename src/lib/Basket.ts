be134b1b15f8aa60a048e1e198a9f2f6a817a0ac
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-05-04 17:45:29 +0100

    Add comments in Basket class

--- src/lib/Basket.ts
+++ src/lib/Basket.ts
@@ -5,21 +5,49 @@ const sumAmounts = (
   result: Basket.RuleResult
 ): Basket.Price => acc + result.amount;
 
+/*
+ * This item is used to ease the accumulator initialisation
+ * in the rules functions. The code value is inspired by redux :)
+ */
 const initialItem: Basket.Item = { code: "@@init", price: 0 };
 
+/*
+ * The Basket class keeps track of the basket items and the price
+ * computation rules.
+ *
+ * It provides a simple api to add items and return the total price.
+ */
 class Basket {
   items: Array<Basket.Item> = [];
-  pricingRules: Array<any>;
+  pricingRules: Array<Basket.Rule>;
 
+  /*
+   * The Basket class is initialised with the list of pricing rules functions.
+   * These functions are reduce functions, aka reducers.
+   */
   constructor(pricingRules = []) {
     this.pricingRules = pricingRules;
   }
 
+  /*
+   * Appends the item to the items array and returns the
+   * basket for ease of use (e.g. chaining)
+   */
   add(item: Basket.Item) {
     this.items.push(item);
     return this;
   }
 
+  /*
+   * Applies the pricing rules to the items list and sums all their amounts
+   *
+   * i.e.:
+   * - each rule reduces the item list to a single amount
+   * - these rules' amounts are summed and reduced to a number
+   *
+   * Notice the dumb initial item `initialItem` used to ease the rules
+   * accumulator initialisation.
+   */
   total() {
     return this.pricingRules
       .map(rule => [initialItem, ...this.items].reduce(rule, { amount: 0 }))
